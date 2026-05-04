44eb7dd38b2d9a3ae5956d71a14032e541a59d6a
unknown <mahmudur.salman@gmail.com>
2026-05-05 03:22:36 +0600

    footer links added

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,9 +1,9 @@
 import { Github, Linkedin, Twitter, Heart } from "lucide-react";
 
 const socialLinks = [
-  { icon: Github, href: "#", label: "GitHub" },
-  { icon: Linkedin, href: "#", label: "LinkedIn" },
-  { icon: Twitter, href: "#", label: "Twitter" },
+  { icon: Github, href: "https://github.com/Mahmudur-Rahman-Salman", label: "GitHub" },
+  { icon: Linkedin, href: "https://www.linkedin.com/in/mahmudur-rahman-salman/", label: "LinkedIn" },
+  
 ];
 
 const footerLinks = [
